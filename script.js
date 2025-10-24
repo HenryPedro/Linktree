7243275dfa936d869e9b5b5e34936be9061ccbdb
Pedro Almeida <henricorreiapedro@gmail.com>
2025-10-24 15:30:02 -0300

    feat: add dark mode toggle

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const toggleButton = document.getElementById('theme-toggle');
 
-toggleButton.addEventListener('click', ()=> {
+if (localStorage.getItem('theme') === 'dark') {
+    document.body.classList.add('dark-mode');
+}
+
+
+toggleButton.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
-})
+    
+    if (document.body.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.removeItem('theme');
+    }
+});
